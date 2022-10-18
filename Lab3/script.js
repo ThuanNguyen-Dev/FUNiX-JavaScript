26dabe072478d21215f9862e8e53581c1860d264
Thuận Nguyễn <thuanntv013@gmail.com>
2022-10-19 00:18:46 +0700

    add lab 3 - lab 4

---
+++ Lab3/script.js
@@ -0,0 +1,127 @@
+//#region 3.1
+
+//* 3.1.1
+/**
+ * ? Chuyển đổi kiểu và ép kiểu 
+'9' - '5'; = 4
+
+'19' - '13' + '17'; = 617
+
+'19' - '13' + 17; = 23
+
+'123' < 57; = false
+
+5 + 6 + '4' + 9 - 4 - 2; = 1143
+*/
+
+//* 3.1.2
+console.log("9" - "5");
+console.log("19" - "13" + "17");
+console.log("19" - "13" + 17);
+console.log("123" < 57);
+console.log(5 + 6 + "4" + 9 - 4 - 2);
+//#endregion 3.1
+
+//#region 3.2
+/**
+ * ? Toán tử bằng: == với ===
+ *1. Khai báo biến 'numNeighbours' dựa trên prompt input sau: prompt('How many neighbor countries does your country have?'). Bạn có thể tìm hiểu cách sử dụng hàm prompt để lấy dữ liệu từ người dùng ở link sau.
+ *2. Nếu chỉ có 1 neighbour, hãy in ra console 'Only 1 border!' (sử dụng ==).
+ *3. Sử dụng else-if block để ghi 'More than 1 border' trong trường hợp 'numNeighbours' lớn hơn 1.
+ *4. Sử dụng else block để ghi 'No borders' (block này sẽ được thực thi khi 'numNeighbours' là 0 hoặc bất kỳ giá trị nào khác).
+ *5. Kiểm tra code với các giá trị 'numNeighbours' khác nhau, gồm 1 và 0.
+ *6. Thay == thành ===, và kiểm tra lại code với các giá trị 'numNeighbours' tương tự. Điều gì sẽ xảy ra khi numNeighbours = 1? Tại sao lại như vậy?
+ ** Đáp án: Khi numNeighbours = 1 thì sẽ không thực hiện được câu lệnh else-if vì nó không thỏa mãn điều kiện. Vì vậy, câu lệnh else sẽ được thực hiện.
+ ** '1' === 1 => false
+ *7. Cuối cùng, chuyển đổi 'numNeighbours' thành một số, và xem điều gì sẽ xảy ra khi bạn nhập vào 1.
+ *8. Hãy nêu lý do chúng ta nên sử dụng toán tử === và chuyển đổi kiểu trong trường hợp này.
+ ** Đáp án: Chúng ta nên sử dụng toán tử === vì nó sẽ so sánh cả giá trị và kiểu dữ liệu của biến. Còn toán tử == chỉ so sánh giá trị của biến.
+ */
+
+/** 
+ * ! vô hiệu hóa code trước đó để prompt không xuất hiện 
+let numNeighbours = Number(
+  prompt("How many neighbor countries does your country have?")
+);
+ 
+ * ? thay toán tử == thành ===
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
+*/
+
+//#endregion 3.2
+
+//#region 3.3
+/**
+ * ? Toán tử logic
+ *1. Hãy vô hiệu hóa code trước đó để prompt không xuất hiện.
+ *2. Giả sử Sarah đang tìm một quốc gia mới để sinh sống. Cô ấy muốn ở một đất nước sử dụng ngôn ngữ Tiếng Anh, dân số ít hơn 50 triệu người và không phải đảo quốc.
+ *3. Bạn cần tạo các biến tương ứng với ngôn ngữ, dân số, có phải đảo quôcs không. Giá trị các biến này sẽ được nhập bằng hàm prompt.
+ *4. Hãy viết một câu lệnh if giúp Sarah tìm kiếm quốc gia phù hợp. Bạn cần viết điều kiện đánh giá  tất cả các tiêu chí của Sarah. Hãy dành thời gian thực hiện điều này.
+ *5. Nếu đất nước nhập vào phù hợp, in ra string như sau: 'You should live in Portugal :)'. Ngược lại, hãy in 'Portugal does not meet your criteria :('
+ *6. Có thể đất nước mà bạn nhập không đáp ứng toàn bộ tiêu chí. Hãy quay trở lại và thay đổi tạm thời một số biến để điều kiện trở nên đúng (trừ khi bạn sống ở Canada).
+ */
+
+let flag = false;
+while (flag == false) {
+  let language = prompt("What language do you speak?", "English");
+  let population = Number(
+    prompt("What is the population of your country?", 49000000)
+  );
+  let isIsland = prompt("Is your country an island?");
+  if (language === "English" && population < 50000000 && !isIsland) {
+    console.log("You should live in Portugal :)");
+    flag = true;
+  } else {
+    console.log("Portugal does not meet your criteria :(");
+  }
+}
+//#endregion 3.3
+
+//#region 3.4
+/**
+ * ? Câu lệnh switch
+ * Sử dụng câu lệnh switch để ghi string sau cho 'language' dựa trên giá trị của 'language':
+ */
+let language = prompt("What language do you speak?", "Mandarin");
+switch (language) {
+  case "Chinese":
+  case "Mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "Spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "English":
+    console.log("3rd place");
+    break;
+  case "Hindi":
+    console.log("Number 4");
+    break;
+  case "Arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+//#endregion 3.4
+
+//#region 3.5
+/**
+ * ? Toán tử điều kiện (ba ngôi)
+ * Nếu dân số của đất nước lớn hơn 33 triệu người, sử dụng toán tử ba ngôi để in string sau ra console: 'Portugal's population is above average'.
+Ngược lại, hãy in 'Portugal's population is below average'. Lưu ý giữa hai câu này chỉ có một từ thay đổi!
+ */
+
+let population = Number(
+  prompt("What is the population of your country?", 33000000)
+);
+population > 33000000
+  ? console.log("Portugal's population is above average")
+  : console.log("Portugal's population is below average");
+//#endregion 3.5
