d9652f16f27988d3207ef5945169dbfc7f25c6ac
Thuận Nguyễn <thuanntv013@gmail.com>
2022-10-20 13:55:32 +0700

    lab 3.6.2 Tip calculator

--- Lab3/script.js
+++ Lab3/script.js
@@ -174,4 +174,29 @@ if (
 } else if (scoreDolphins2 < scoreKoalas2 && scoreKoalas2 >= 100) {
   console.log("Koalas win the trophy!");
 }
+
+/**
+ * ? 3.6.2 Tip
+ * Steven muốn tạo một tip calculator (công cụ tính tiền tip) đơn giản cho những khi anh ta muốn ăn ở nhà hàng.
+ * Ở đất nước của anh ta, người ta thường tip 15% nếu giá trị hóa đơn nằm trong khoảng 50-300. Với những giá trị khác, tip thường là 20%.
+ * ? Nhiệm vụ của bạn:
+ * Tính tip dựa theo giá trị hóa đơn. Tạo biến 'tip' cho điều này. Không dùng câu lệnh if/else
+ * (Để dễ hơn, bạn có thể bắt đầu với lệnh if/else sau đó chuyển đổi nó thành toán tử ba ngôi!)
+ * In string ra console có chứa giá trị hóa đơn (bill), tiền tip và giá trị cuối cùng (bill + tip).
+ * Ví dụ: “The bill was 275, the tip was 41.25, and the total value 316.25”
+ * * Dữ liệu kiểm tra:
+ * Dữ liệu 1: Kiểm tra giá trị bill 275, 40 và 430
+ * !Gợi ý:
+ * Tính 20% của giá trị, nhân nó với 20/100 = 0.2
+ * Giá trị X nằm trong khoảng 50 và 300, nếu nó >= 50 && <= 300
+ */
+
+const bill = [275, 40, 430];
+for (let i = 0; i < bill.length; i++) {
+  const tip = bill[i] >= 50 && bill[i] <= 300 ? bill[i] * 0.15 : bill[i] * 0.2;
+  const total = tip + bill[i];
+  console.log(
+    `The bill was ${bill[i]}, the tip was ${tip}, and the total value ${total}`
+  );
+}
 //#endregion 3.6
