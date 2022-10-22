1c32700bf33a0a7eebb132571f5f4187e14da552
Thuận Nguyễn <thuanntv013@gmail.com>
2022-10-22 09:16:18 +0700

    Done Lab5

--- Lab5/script.js
+++ Lab5/script.js
@@ -66,7 +66,7 @@ for (let i = 1; i <= 50; i++) {
 }
 
 /**
- * ? Lab 5.5. Lặp trong array, break và continue (15 phút)
+ * ? Lab 5.5. Lặp trong array, break và continue
  * Hãy lấy lại array 'populations' từ bài lab trước.
  * Sử dụng vòng lặp for để tạo array 'percentages2' chứa phần trăm dân số thế giới cho 4 giá trị population. Sử dụng hàm 'percentageOfWorld1' mà bạn đã tạo trước đó.
  * Xác nhận rằng 'percentages2' chứa chính xác các giá trị trong array 'percentages' mà chúng ta đã tạo theo cách thủ công ở lab trước, để xem giải pháp này tốt hơn như thế nào.
@@ -79,4 +79,118 @@ for (let i = 0; i < populations.length; i++) {
 }
 console.log(percentages2);
 
+/**
+ * ? Lab 5.6. Looping Backwards and Loops in Loops(Vòng lặp ngược và Vòng lặp trong vòng lặp)
+ * Lưu trữ array của những array này vào biến 'listOfNeighbours' [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+ * Hiển thị các quốc gia láng giềng vào màn hình console, không phải toàn bộ array. Ví dụ 'Neighbour: Canada' cho từng quốc gia.
+ * Bạn sẽ cần một vòng lặp bên trong vòng lặp cho điều này. Điều này thực sự hơi phức tạp, vì vậy đừng lo lắng nếu nó quá khó đối với bạn! Bạn sẽ giải quyết được vấn đề này.
+ */
+
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+  }
+}
+
+/**
+ * ? 5.7. Vòng lặp While
+ * Với đề bài tương tự Lab 5.5 Lặp trong array, break và continue, nhưng lần này hãy sử dựng vòng lặp while (gọi array 'percentages3').
+ * Bạn thích giải pháp nào hơn cho nhiệm vụ này: vòng lặp for hay vòng lặp while?
+ */
+
+const percentages3 = [];
+let i = 0;
+while (i < populations.length) {
+  percentages3.push(percentageOfWorld1(populations[i]));
+  i++;
+}
+console.log(percentages3);
+
+/**
+ * ? Lab 5.8: Bài toán tổng hợp #4
+ */
+/**
+ * ? Lab 5.8.1. So sánh chỉ số IBM (phần 3)
+ * * Nhiệm vụ của bạn:
+ * Hãy quay lại ví dụ so sánh chỉ số BMI của Mark và John! Lần này, hãy dùng object để triển khai các phép tính! Nhớ rằng: BMI = mass/[(height)^2)] = mass/(height*height) (mass tính bằng kg và height tính bằng mét)
+ * Với mỗi người (Mark Miller và John Smith), hãy tạo một object có các thuộc tính như full name, mass, and height
+ * Tạo phương thức 'calcBMI' ở mỗi object để tính BMI (phương thức như nhau ở cả hai object). Lưu giá trị BMI vào một thuộc tính và trả về từ phương thức.
+ * In ra console người có BMI cao hơn cùng với tên đầy đủ và BMI tương ứng. Ví dụ: "John's BMI (28.3) is higher than Mark's (23.9)!"
+ * * Dữ liệu
+ * Marks nặng 78 kg và cao 1m69. John nặng 92kg và cao 1m95.
+ */
+
+const Marks = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+const John = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+if (Marks.calcBMI() > John.calcBMI()) {
+  console.log(
+    `${Marks.fullName}'s BMI (${Marks.bmi}) is higher than ${John.fullName}'s (${John.bmi})!`
+  );
+} else if (Marks.bmi < John.bmi) {
+  console.log(
+    `${John.fullName}'s BMI (${John.bmi}) is higher than ${Marks.fullName}'s (${Marks.bmi})!`
+  );
+}
+
+/**
+ * ? Lab 5.8.2. Cải thiện Tip calculator
+ * Hãy cải thiện thêm tip calculator của Steven, lần này sử dụng các vòng lặp!
+ * * Nhiệm vụ của bạn:
+ * Tạo array 'bills' chứa tất cả 10 giá trị hóa đơn kiểm tra.
+ * Tạo các array rỗng cho 'tips' và 'totals'.
+ * Sử dụng hàm 'calcTip' mà chúng ta đã viết trước đó (không cần lặp lại) để tính các giá trị tips và totals (hóa đơn + tiền boa) cho mỗi giá trị bill trong bills array.
+ * Sử dụng vòng lặp for để thực hiện 10 phép tính!.
+ * *Dữ liệu kiểm tra:
+ * 22, 295, 176, 440, 37, 105, 10, 1100, 86 và 52
+ * * Gợi ý: Gọi ‘calcTip‘ trong vòng lặp và dùng phương thức push để thêm giá trị cho các array tips và totals.
+ * * Bonus: Viết hàm 'calcAverage' nhận array 'arr' làm đối số. Hàm này tính trung bình tất cả các số đã cho trong array.
+ * * Đây là một thử thách khó (chúng ta chưa từng thực hiện điều này trước đây)! Dưới đây là hướng dẫn thực hiện:
+ * Trước tiên, bạn cần cộng tất cả các giá trị trong array. Để thực hiện phép cộng, tạo biến 'sum' bắt đầu từ 0.
+ * Sau đó lặp qua array, sử dụng vòng lặp for. Ở mỗi lần lặp, cộng giá trị hiện tại vào biến 'sum'. Như vậy, ở cuối vòng lặp, bạn sẽ cộng được tất cả các giá trị với nhau.
+ * Để tính trung bình, chia tổng mà bạn vừa tính trước đó cho độ dài của array (vì đó là số phần tử).
+ * Gọi hàm với array 'totals'.
+ */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+const calctips = (bill) => {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+for (let i = 0; i < bills.length; i++) {
+  const tip = calctips(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log("Bills:" + bills + ", TIPS:" + tips + ", TOTAL:" + totals);
+
+const calcAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage(totals));
 //#endregion Lab 5
