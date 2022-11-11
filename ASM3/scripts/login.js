6db5cd56fe8c3095337f4057f84f58f5f43df598
Thuận Nguyễn <thuanntv013@gmail.com>
2022-11-12 01:08:14 +0700

    ASM3: Done User, login, register

--- ASM3/scripts/login.js
+++ ASM3/scripts/login.js
@@ -1,1 +1,49 @@
-'use strict'
+"use strict";
+
+const min_pass = 8;
+const KEY = "USER_ARRAY";
+const userArr = JSON.parse(getFromStorage(KEY)) || [];
+
+const submitBtn = document.getElementById("btn-submit");
+const username = document.getElementById("input-username");
+const password = document.getElementById("input-password");
+
+submitBtn.addEventListener("click", (e) => {
+  if (!validate()) return;
+  e.preventDefault();
+  for (let user in userArr) {
+    console.log(userArr[user].firstName);
+    if (
+      userArr[user].username === username.value &&
+      userArr[user].password === password.value
+    ) {
+      const curent_user = {
+        firstName: userArr[user].firstName,
+        lastName: userArr[user].lastName,
+        username: userArr[user].username,
+        password: userArr[user].password,
+      };
+      console.log(curent_user);
+      saveToStorage("currentUser", JSON.stringify(curent_user));
+      window.location.href = "../index.html";
+    }
+  }
+});
+
+const validate = function () {
+  if (username.value === "") {
+    alert("Please enter your username");
+    return false;
+  }
+
+  if (password.value === "") {
+    alert("Please enter your password");
+    return false;
+  }
+  if (password.value.length < min_pass) {
+    alert(`Password must be at least ${min_pass} characters`);
+    return false;
+  }
+
+  return true;
+};
--- ASM3/scripts/register.js
+++ ASM3/scripts/register.js
@@ -1,1 +1,84 @@
-'use strict'
+"use strict";
+
+const min_pass = 8;
+const KEY = "USER_ARRAY";
+const userArr = JSON.parse(getFromStorage(KEY)) || [];
+
+const submitBtn = document.getElementById("btn-submit");
+const firstName = document.getElementById("input-firstname");
+const lastName = document.getElementById("input-lastname");
+const username = document.getElementById("input-username");
+const password = document.getElementById("input-password");
+const passwordConfirm = document.getElementById("input-password-confirm");
+
+submitBtn.addEventListener("click", (e) => {
+  if (!validate()) return;
+  e.preventDefault();
+  const user = new User(
+    firstName.value,
+    lastName.value,
+    username.value,
+    password.value
+  );
+  userArr.push(user);
+  saveToStorage(KEY, JSON.stringify(userArr));
+  window.location.href = "login.html";
+});
+
+const validate = function () {
+  if (firstName.value === "") {
+    alert("Please enter your first name");
+    return false;
+  }
+  if (lastName.value === "") {
+    alert("Please enter your last name");
+    return false;
+  }
+  if (username.value === "") {
+    alert("Please enter your username");
+    return false;
+  }
+
+  for (let user in userArr) {
+    if (userArr[user].username === username.value) {
+      alert("Username is the same as an existing username!");
+      return false;
+    }
+  }
+
+  if (password.value === "") {
+    alert("Please enter your password");
+    return false;
+  }
+  if (password.value.length < min_pass) {
+    alert(`Password must be at least ${min_pass} characters`);
+    return false;
+  }
+  if (passwordConfirm.value === "") {
+    alert("Please confirm your password");
+    return false;
+  }
+
+  if (passwordConfirm.value.length < 8) {
+    alert(`Password confirm must be at least ${min_pass} characters
+        `);
+    return false;
+  }
+  if (password.value !== passwordConfirm.value) {
+    alert("Passwords do not match");
+    return false;
+  }
+
+  return true;
+};
+
+function parseUser(userData) {
+  const user = new User(
+    userData.firstname,
+    userData.lastname,
+    userData.username,
+    userData.password
+  );
+
+  return user;
+}
