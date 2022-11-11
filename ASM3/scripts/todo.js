567f5a1ebc6d08977db2d5a60f47d46bb72637e1
Thuận Nguyễn <thuanntv013@gmail.com>
2022-11-12 01:12:01 +0700

    Add TODO ASM3

--- ASM3/scripts/todo.js
+++ ASM3/scripts/todo.js
@@ -1,1 +1,15 @@
-'use strict'
+"use strict";
+
+// TODO: Home Page
+// TODO: Chức năng Logout
+// TODO: Hiển thị các bài viết
+// TODO: Chuyển trang cho các bài viết
+/**
+ * TODO: Hiển thị Todo List
+ * 1. Thêm mới Todo và Lưu dữ liệu vào LocalStorage.
+ * 2. Hiển thị các Task
+ * 3. Toggle Task
+ * 4. Delete Task
+ */
+// TODO: Thay đổi thiết lập
+// TODO: (Nâng cao) Tìm kiếm bài viết theo từ khóa
