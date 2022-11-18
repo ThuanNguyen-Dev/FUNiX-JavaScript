9e5e13c615e1d227d4c992802cc5dc89cb418a3e
Thuận Nguyễn <thuanntv013@gmail.com>
2022-11-18 15:12:18 +0700

    update

--- Lab15/script.js
+++ Lab15/script.js
@@ -2,27 +2,43 @@
 
 const image = document.querySelector(".images");
 
-const createImage = function (imgPath) {
+const createImage = async function (imgPath) {
   const img = document.createElement("img");
   img.src = imgPath;
   img.className = "image";
   return img;
 };
 
-const img1 = createImage("img/img-1.jpg");
-const img2 = createImage("img/img-2.jpg");
-const img3 = createImage("img/img-3.png");
+let wait = function (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-const images = [img1, img2, img3];
-
-const loadAll = async function (imgArr) {
+const loadNPause = async function () {
   try {
-    const imgs = await Promise.all(imgArr);
-    console.log(imgs);
-    imgs.forEach((img) => image.append(img));
+    let img = await createImage("img/img-1.jpg");
+    image.append(img);
+    await wait(2000);
+    await wait(2);
+    img.style.display = "none";
+    img = await createImage("img/img-2.jpg");
+    image.append(img);
+    await wait(2000);
+    img.style.display = "none";
   } catch (err) {
     console.error(err);
   }
 };
 
-loadAll(images);
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async (img) => await createImage(img));
+    const imgEl = await Promise.all(imgs);
+    imgEl.forEach((img) => image.append(img));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.png"]);
